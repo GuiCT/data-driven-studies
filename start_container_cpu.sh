5124c8e5dac3d2392d0bffa5ac5c7069991035eb
GuiCT <gtomiasi@gmail.com>
2023-07-12 21:27:42 -0300

    fix: requirements; feat: scripts para Docker

---
+++ start_container_cpu.sh
@@ -0,0 +1,1 @@
+docker run --name "dds_cpu" -v "$(pwd):/workspace" -it tensorflow/tensorflow:latest /bin/bash
---
+++ start_container_gpu.sh
@@ -0,0 +1,1 @@
+docker run --gpus all --name "dds_gpu" -v "$(pwd):/workspace" -it tensorflow/tensorflow:latest-gpu-jupyter /bin/bash
